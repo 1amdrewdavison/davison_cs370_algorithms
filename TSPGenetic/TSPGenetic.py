b9855a47da2ed0bd737af6c9060ea7addc70b108
Andrew Davison <02.andrew.davison@gmail.com>
2023-05-04 15:55:28 -0400

    Distance bug and Child generation bug

--- TSPGenetic/TSPGenetic.py
+++ TSPGenetic/TSPGenetic.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
+num_cities = int(input("Enter a number of cities:"))
+num_generations = int(input("Enter a number of generations:"))
+generation_size = 10
+
 def determine_fitness(tour):
     total = 0
     for i in tour:
@@ -9,12 +13,39 @@ def determine_fitness(tour):
             total += np.linalg.norm(tour[i]-tour[0])
         else:
             total += np.linalg.norm(tour[i]-tour[i+1])   
-    return total     
+    return total   
 
-num_cities = int(input("Enter a number of cities:"))
-num_generations = int(input("Enter a number of generations:"))
-generation_size = 100
-
+def make_child(parent1, parent2, cities):
+    vertexes_left = True
+    start_vertex = 0
+    
+    while(vertexes_left): 
+        child = []
+        child.append(start_vertex)
+        
+        for i in range(1, num_cities):
+            p1_next_vertex = parent1[(parent1.index(start_vertex) + 1) % num_cities]
+            p2_next_vertex = parent2[(parent2.index(start_vertex) + 1) % num_cities]
+            p1_edge = np.linalg.norm(cities[start_vertex] - cities[p1_next_vertex])
+            p2_edge = np.linalg.norm(cities[start_vertex] - cities[p2_next_vertex])
+            
+            if (p1_edge > p2_edge and not p2_next_vertex in child):
+                start_vertex = p2_next_vertex
+                child.append(p2_next_vertex)
+            elif (not p1_next_vertex in child):
+                start_vertex = p1_next_vertex
+                child.append(p1_next_vertex)
+            else:
+                start_vertex += 1
+                if (start_vertex == len(parent1)):
+                    vertexes_left = False
+                break
+        
+        if (vertexes_left and len(child) == len(parent1)):
+            break
+        
+    return child
+  
 # Create a NumPy random generator object
 rng = np.random.default_rng(seed=42) 
 # ^^ Set seed to 42, for repeatability when testing. Remove when debugging is complete
@@ -24,24 +55,48 @@ cities = rng.normal(0, 1, (num_cities, 2))
     
 curr_generation = []
 curr_fitness = []
+best_fits = []
 
 #Randomly generate first generation
 for i in range(generation_size):
     curr_generation.append(random.sample(range(num_cities), num_cities))
     curr_fitness.append(determine_fitness(curr_generation[-1]))
+
+best_fits.append(sorted(curr_fitness)[0])
+
+#run genetic algorithm
+for i in range(1, num_generations):
+    #grab the indexes of the top 50% most fit
+    best_tours = sorted(range(len(curr_fitness)), key=lambda i: curr_fitness[i])[:(int)(generation_size/2)]
+    parent_pool = []
     
+    #insert parents into pool
+    for i in best_tours:
+        parent_pool.append(curr_generation[i])
+        parent_pool.append(curr_generation[i])
+        parent_pool.append(curr_generation[i])
+        parent_pool.append(curr_generation[i])
+    
+    #make next generation
+    curr_generation = []
+    curr_fitness = []
+    while (parent_pool):
+        parents = random.sample(parent_pool, 2)
+        parent_pool.remove(parents[0])
+        parent_pool.remove(parents[1])
+        child = make_child(parents[0], parents[1], cities)
+        curr_generation.append(child)
+        curr_fitness.append(determine_fitness(child))
+        
+    best_fits.append(sorted(curr_fitness)[0])
 
-#for i in range(num_generations):
+print("Best fit path weight:", best_fits[-1])
 
-print(cities)
-print(curr_generation[0])
-print(curr_fitness[0])    
-
-x_data = range(generation_size)
-y_data = curr_fitness
+x_data = range(num_generations)
+y_data = best_fits
 
 # labels = ['First column description', 'Second column description']
-labels = ['tours', 'fitness']
+labels = ['improvement']
 
 plt.plot(x_data, y_data, linestyle='-', marker='o')
 plt.legend(labels)
